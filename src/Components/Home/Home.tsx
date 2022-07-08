d25d4d1e6da947f5342db61cd47d0c36fdf65f95
Jason Ngan <jason.ngan@shopee.com>
2022-07-08 11:38:03 +0800

    add sidebar toggler

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,27 +1,48 @@
 import { Layout } from "antd";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useWindowDimensions } from "../../_shared/hooks/useWindowDimensions";
 import Chatroom from "./Chatroom/Chatroom";
 import Nav from "./Nav/Nav";
 import Sidebar from "./Sidebar/Sidebar";
 
-const { Header, Sider, Content } = Layout;
+const { Sider, Content } = Layout;
 
-const Home = (): JSX.Element => (
-  <>
-    <Layout className="home">
-      <Sider width={'18rem'} className="bg-one sider">
-        <Sidebar />
-      </Sider>
-      <Layout className="body">
-        <Header className="bg-two header shadow">
-          <Nav />
-        </Header>
-        <Content className="bg-two content">
-          <Chatroom></Chatroom>
-        </Content>
+const Home = (): JSX.Element => {
+  const { width } = useWindowDimensions()
+  const [ showSideBar, setShowSideBar ] = useState(width > 600);
+
+  useEffect(() => {
+    if (width < 600) {
+      setShowSideBar(false)
+    } else {
+      setShowSideBar(true)
+    }
+  }, [width])
+
+  const toggleSideBar = () => {
+    setShowSideBar(!showSideBar)
+  }
+
+  return (
+    <>
+      <Layout className="home">
+        <Sider
+          width={"18rem"}
+          className={`bg-one sider ${!showSideBar ? "disable" : ""}`}
+        >
+          <Sidebar />
+        </Sider>
+        <Layout className="body">
+          <div className="bg-two header shadow">
+            <Nav toggleSideBar={toggleSideBar} />
+          </div>
+          <Content className="bg-two content">
+            <Chatroom></Chatroom>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
-  </>
-);
+    </>
+  );
+};
 
 export default Home;
--- src/Components/Home/Nav/Nav.tsx
+++ src/Components/Home/Nav/Nav.tsx
@@ -2,13 +2,22 @@
 import React from "react";
 
 import Text from "../../../_shared/Text/Text";
+import { MenuOutlined } from "@ant-design/icons";
 
-const Nav = (): JSX.Element => (
-  <div className="nav">
-    <Text size="1.1rem" bd="700" align="left">
-      FRONT-END DEVELOPERS
-    </Text>
-  </div>
-);
+type NavProps = {
+  toggleSideBar: () => void;
+};
+const Nav = (props: NavProps): JSX.Element => {
+  const { toggleSideBar } = props
+
+  return (
+    <div className="nav">
+      <MenuOutlined className="icon secondary" onClick={toggleSideBar} />
+      <Text size="1.1rem" bd="700" align="left">
+        FRONT-END DEVELOPERS
+      </Text>
+    </div>
+  );
+};
 
 export default Nav;
